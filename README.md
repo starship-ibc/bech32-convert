5e050d97191c7bb9cb63103c474c64520a87a6a0
Starship IBC <shrugs.rest0x@icloud.com>
2022-12-11 21:27:38 -0500

    Single address conversion

--- README.md
+++ README.md
@@ -10,8 +10,25 @@ go install
 
 ## Usage
 
+You can convert either convert an individual address or a file containing a list of addresses.
+
+### Convert an address
+
+```sh
+bech32-convert address chain
+```
+
+Example:
+
+```sh
+$ bech32-convert cosmos1z6mj02l2s8v0vsxfsark5v7t076ds8pu309hza stars
+cosmos1z6mj02l2s8v0vsxfsark5v7t076ds8pu309hza,stars1z6mj02l2s8v0vsxfsark5v7t076ds8pu9nj2fv
+```
+
+### Convert a file of addresses
+
 You can convert any text file containing a list of addresses:
 
 ```sh
-bech32-convert addrs.txt chain
+bech32-convert addrs_file chain
 ```
--- main.go
+++ main.go
@@ -9,27 +9,36 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/bech32"
 )
 
-func main() {
-
-	f, err := os.Open(os.Args[1])
-
+func convert(addr string, target string) string {
+	_, bz, err := bech32.DecodeAndConvert(addr)
 	if err != nil {
 		log.Fatal(err)
 	}
+	
+	convertedAddr, err := bech32.ConvertAndEncode(target, bz)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return convertedAddr
+}
+
+func main() {
+	addr := os.Args[1]
+	target := os.Args[2]
+
+	f, err := os.Open(addr)
+	if err != nil {
+		convertedAddr := convert(addr, target)
+		fmt.Printf("%s,%s\n", addr, convertedAddr)
+	}
+	
 	defer f.Close()
 	scanner := bufio.NewScanner(f)
 	scanner.Split(bufio.ScanLines)
 
 	for scanner.Scan() {
 		addr := scanner.Text()
-		_, bz, err := bech32.DecodeAndConvert(addr)
-		if err != nil {
-			log.Fatal(err)
-		}
-		convertedAddr, err := bech32.ConvertAndEncode(os.Args[2], bz)
-		if err != nil {
-			log.Fatal(err)
-		}
+		convertedAddr := convert(addr, target)
 		fmt.Printf("%s,%s\n", addr, convertedAddr)
 	}
 }
